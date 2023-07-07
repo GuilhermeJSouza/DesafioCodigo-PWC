a5afecd15c48b4460304340096a355f206b99eb5
GuilhermeJSouza <gjds360@hotmail.com>
2023-07-07 14:05:19 -0300

    Primeira Parte do Método main

--- ManipulationString.java
+++ ManipulationString.java
@@ -1,5 +1,47 @@
 //ManipulationString.java
 //Creator: GuilhermeJSouza
 //Date: 07/07/2023
-//Código para "Desafio de Código: Manipulação de Strings" do processo seletivo da empresa PWC para a vaga de estágio em Tecnologia.
+//Description: Código para "Desafio de Código: Manipulação de Strings" do processo seletivo da empresa PWC para a vaga de estágio em Tecnologia.
 
+import java.util.Scanner;
+
+public class ManipulationString
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("""
+        
+        Digite o número correspondente à operação a ser realizada:
+
+        1  Reverter ordem
+        2  Remover duplicata
+        3  Encontre Palíndromo
+        4  Iniciais maiúsculas
+        5  Anagrama""");
+
+        int comando = input.nextInt();
+
+        if (comando >= 1 && comando <= 5)
+        {
+            switch (comando)
+            {
+                case 1:
+                    System.out.println("Digite a frase a ser revertida.");
+                    String limpa = input.nextInt();
+                    String frase = input.nextLine();
+                    reverta(frase);
+                    break;
+            }
+        }
+
+        
+    }
+
+    public static String reverta(String fraseRevertida)
+    {
+        
+        return fraseRevertida;
+    }
+}
