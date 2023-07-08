b562f352d67bfca9fe734f7ce990326214cb2193
GuilhermeJSouza <gjds360@hotmail.com>
2023-07-07 22:03:14 -0300

    Método Remover Letras Duplicadas

--- ManipulationString.java
+++ ManipulationString.java
@@ -30,7 +30,7 @@ public class ManipulationString {
         {
             switch (comando) // Escolhe a execução de acordo com o comando do usuário
             {
-                case 1: // Comando REVERTER
+                case 1: // Comando reverter ordem
 
                     System.out.println("Digite a frase a ser revertida."); // Comunica ao usuário o próximo comando
 
@@ -39,6 +39,19 @@ public class ManipulationString {
                     String fraseNova = reverta(frase); // Executa o comando especificado
 
                     System.out.println(fraseNova);
+
+                    break;
+
+                case 2: // Comando remover duplicata
+
+                    System.out.println("Digite a frase para remover caracteres duplicados."); // Comunica ao usuário o
+                                                                                              // próximo comando
+
+                    String frase2 = input.nextLine(); // Lê a entrada do usuário
+
+                    String fraseNova2 = RemoveDuplicata(frase2); // Executa o comando especificado
+
+                    System.out.println(fraseNova2);
 
                     break;
             }
@@ -67,20 +80,55 @@ public class ManipulationString {
                 separadores.add(frase.indexOf(" ", x));
             }
         }
-    
-        // PALAVRAS QUE COMPÕEM A FRASE
-        palavras.set(0, frase.substring(0, separadores.get(0)));
 
-        for (int y = 1; y < separadores.size(); y++) {
+        // PALAVRAS QUE COMPÕEM A FRASE
+        palavras.set(0, frase.substring(0, separadores.get(0))); // Insere a primeira palavra
+
+        for (int y = 1; y < separadores.size(); y++) { // Insere as palavras do meio
             palavras.add(frase.substring(separadores.get(y - 1) + 1, separadores.get(y)));
         }
 
-        palavras.add(frase.substring(separadores.get(separadores.size() - 1) + 1, frase.length()));
+        palavras.add(frase.substring(separadores.get(separadores.size() - 1) + 1, frase.length())); // Insere a última
+                                                                                                    // palavra
 
-        for (int z = palavras.size(); z > 0; z--) {
+        for (int z = palavras.size(); z > 0; z--) { // Concatena as palavras
             fraseRevertida += palavras.get(z - 1) + " ";
         }
 
         return fraseRevertida;
+    }
+
+    public static String RemoveDuplicata(String frase2) {
+        ArrayList<String> letras = new ArrayList<String>(); // Cria um arraylist para as letras da frase
+
+        for (int w = 0; w < frase2.length(); w++) // Coloca cada letra da frase no vetor
+        {
+            letras.add(frase2.substring(w, w + 1));
+        }
+
+        for (int x = 0; x < letras.size() - 1; x++) {
+            // System.out.printf("%n X %s", letras.get(x)); // teste
+
+            for (int y = x + 1; y < letras.size(); y++) {
+                // System.out.printf(" Y %s", letras.get(y)); // teste
+
+                if (letras.get(y).equals(letras.get(x))) {
+                    if (!letras.get(y).equals(" ")) {
+                        letras.remove(y);
+                    }
+                }
+            }
+
+        }
+
+        // System.out.println(letras); // teste
+
+        String fraseSemDuplicata = "";
+
+        for (String letra : letras) {
+            fraseSemDuplicata += letra;
+        }
+
+        return fraseSemDuplicata;
     }
 }
